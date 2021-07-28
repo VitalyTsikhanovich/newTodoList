e86dfc844aa7a05400d76320c62cd108641b1f19
tihon <tihonnet@gmail.com>
2021-07-29 01:17:08 +0300

    feat: add

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,37 +1,43 @@
-import React from 'react';
-import './App.css';
-import {AppBar, Button, Container, IconButton, LinearProgress, Toolbar, Typography} from "@material-ui/core";
-import {Menu} from "@material-ui/icons";
-import {TodoListList} from "../features/TodoListsList/TodoListList";
-import {ErrorSnackBar} from "../components/ErrorSnackBar/ErrorSnackBar";
-import {useSelector} from "react-redux";
-import {AppRootStateType} from "./store";
-import {RequestStatusType} from "./app-reduser";
-
+import React from 'react'
+import './App.css'
+import {
+  AppBar,
+  Button,
+  Container,
+  IconButton,
+  LinearProgress,
+  Toolbar,
+  Typography,
+} from '@material-ui/core'
+import { Menu } from '@material-ui/icons'
+import { TodoListList } from '../features/TodoListsList/TodoListList'
+import { ErrorSnackBar } from '../components/ErrorSnackBar/ErrorSnackBar'
+import { useSelector } from 'react-redux'
+import { AppRootStateType } from './store'
+import { RequestStatusType } from './app-reduser'
 
 function App() {
-  let status = useSelector<AppRootStateType, RequestStatusType>(state =>state.app.status)
-    return (
-        <div className="App">
-            <ErrorSnackBar/>
-            <AppBar position="static">
-
-                <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                        <Menu/>
-                    </IconButton>
-                    <Typography variant="h6">
-                        Todolist
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-                {status === 'loading' && <LinearProgress />}
-            </AppBar>
-            <Container fixed>
-                <TodoListList/>
-            </Container>
-        </div>
-    );
+  let status = useSelector<AppRootStateType, RequestStatusType>(
+    (state) => state.app.status
+  )
+  return (
+    <div className="App">
+      <ErrorSnackBar />
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <Menu />
+          </IconButton>
+          <Typography variant="h6">Todolist</Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+        {status === 'loading' && <LinearProgress />}
+      </AppBar>
+      <Container fixed>
+        <TodoListList />
+      </Container>
+    </div>
+  )
 }
 
-export default App;
+export default App
