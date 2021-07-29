bb8b1c73a0b5d452d4cb57b9d41997e88d6b3a54
tihon <tihonnet@gmail.com>
2021-07-29 12:11:48 +0300

    refactor: gitignore

--- src/components/AddItemForm/AddItemForm.tsx
+++ src/components/AddItemForm/AddItemForm.tsx
@@ -1,62 +1,62 @@
-import React, {ChangeEvent, useState} from "react";
-import { IconButton, TextField} from "@material-ui/core";
-import { AddBox} from "@material-ui/icons";
+import React, { ChangeEvent, useState } from 'react'
+import { IconButton, TextField } from '@material-ui/core'
+import { AddBox } from '@material-ui/icons'
 
 export type AddItemFormPropsType = {
-    addItem: (title: string ) => void
+  addItem: (title: string) => void
 }
 
-export const AddItemForm = React.memo((props: AddItemFormPropsType) =>{
-
-    let [title, setTitle] = useState('')
-    let [error, setError] = useState<string | null>(null)
-    let onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {  // получаем данные из input
-        setTitle(event.currentTarget.value)
-        setError(null)
+export const AddItemForm = React.memo((props: AddItemFormPropsType) => {
+  let [title, setTitle] = useState('')
+  let [error, setError] = useState<string | null>(null)
+  let onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    // получаем данные из input
+    setTitle(event.currentTarget.value)
+    setError(null)
+  }
+  const addTask = () => {
+    if (title.trim() !== '') {
+      //убранными с двух сторон пробелами (trim)
+      props.addItem(title)
+      setTitle('') //добавить таску
+    } else {
+      setError('Title is required')
     }
-    const addTask = () => {
-        if (title.trim() !== '') {                           //убранными с двух сторон пробелами (trim)
-            props.addItem(title)
-            setTitle('')                              //добавить таску{
-        } else {
-            setError('Title is required');
-        }
-    }
+  }
 
-    // const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-    //     if (error !== null) {
-    //         setError(null);
-    //     }
-    //     if (e.charCode === 13) {
-    //         addItem();
-    //     }
-    // }
+  // const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+  //     if (error !== null) {
+  //         setError(null);
+  //     }
+  //     if (e.charCode === 13) {
+  //         addItem();
+  //     }
+  // }
 
-    // let onPressHandler = (event: KeyboardEvent<HTMLInputElement>)=>{
-// if (event.charCode === 13){
-//     props.addTask
-// }
-// }
+  // let onPressHandler = (event: KeyboardEvent<HTMLInputElement>)=>{
+  // if (event.charCode === 13){
+  //     props.addTask
+  // }
+  // }
 
-    return (
-
-        <div>
-            <TextField variant={'outlined'}
-                       value={title}
-                   onChange={onChangeHandler}
-                       label={'Title'}
-                       helperText={error}
-                       error={!!error}
-            />
-            <IconButton color={'primary'} onClick={addTask}>
-            <AddBox/>
-            </IconButton>
-            {/*<Button variant={'contained'} color={'primary'} onClick={addTask}> +</Button>*/}
-            {/*<button onClick={addTask}> +</button>*/}
-            {/*{error &&*/}
-            {/*// <div className='error-message'> {error}</div>*/}
-            {/*}*/}
-        </div>
-
-    )
+  return (
+    <div>
+      <TextField
+        variant={'outlined'}
+        value={title}
+        onChange={onChangeHandler}
+        label={'Title'}
+        helperText={error}
+        error={!!error}
+      />
+      <IconButton color={'primary'} onClick={addTask}>
+        <AddBox />
+      </IconButton>
+      {/*<Button variant={'contained'} color={'primary'} onClick={addTask}> +</Button>*/}
+      {/*<button onClick={addTask}> +</button>*/}
+      {/*{error &&*/}
+      {/*// <div className='error-message'> {error}</div>*/}
+      {/*}*/}
+    </div>
+  )
 })
--- src/features/TodoListsList/TodoList/Task/Task.tsx
+++ src/features/TodoListsList/TodoList/Task/Task.tsx
@@ -1,55 +1,72 @@
-import {useDispatch} from "react-redux";
-import React, {ChangeEvent, useCallback} from "react";
-import {Checkbox, IconButton} from "@material-ui/core";
-import {Delete} from "@material-ui/icons";
-import {TaskStatuses, TaskType} from "../../../../api/todoList-api";
-import {EditableSpan} from "../../../../components/EditableSpan/EditableSpan";
-import {removeTaskTS, updateTaskTC} from "../../tasks-reducer";
-
+import { useDispatch } from 'react-redux'
+import React, { ChangeEvent, useCallback } from 'react'
+import { Checkbox, IconButton } from '@material-ui/core'
+import { Delete } from '@material-ui/icons'
+import { TaskStatuses, TaskType } from '../../../../api/todoList-api'
+import { EditableSpan } from '../../../../components/EditableSpan/EditableSpan'
+import { removeTaskTS, updateTaskTC } from '../../tasks-reducer'
 
 type TaskPropsType = {
-    t: TaskType
-    todoListId: string
+  t: TaskType
+  todoListId: string
 }
 export const Task = React.memo((props: TaskPropsType) => {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-    let onRemoveHandler = useCallback(() => {                //удаление таски
-        const thunk = removeTaskTS(props.t.id, props.todoListId);
-        dispatch(thunk);
+  let onRemoveHandler = useCallback(() => {
+    //удаление таски
+    const thunk = removeTaskTS(props.t.id, props.todoListId)
+    dispatch(thunk)
+  }, [])
 
-    }, [])
+  // let onRemoveHandler = () => dispatch(removeTaskAC(props.t.id, props.todoListId))
+  // let onRemoveHandler = useCallback(()=>props.removeTask(props.t.id, props.todoListId),[props.t.id, props.todoListId])
 
-    // let onRemoveHandler = () => dispatch(removeTaskAC(props.t.id, props.todoListId))
-    // let onRemoveHandler = useCallback(()=>props.removeTask(props.t.id, props.todoListId),[props.t.id, props.todoListId])
+  let onChangeStatusHandler = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      //  контролируемый чекбокс
+      let newIsDoneValue = event.currentTarget.checked
+      let thunk = updateTaskTC(
+        props.t.id,
+        { status: newIsDoneValue ? TaskStatuses.Completed : TaskStatuses.New },
+        props.todoListId
+      )
+      dispatch(thunk)
+      // dispatch(changeTaskStatusAC(props.t.id, newIsDoneValue ? TaskStatuses.Completed : TaskStatuses.New, props.todoListId))
+      // props.changeTaskStatus(props.t.id, newIsDoneValue ? TaskStatuses.Completed : TaskStatuses.New, props.todoListId)
+    },
+    [props.t.id, props.todoListId]
+  )
 
+  let onChangeTitleHandler = useCallback(
+    (newValue: string) => {
+      let thunk = updateTaskTC(
+        props.t.id,
+        { title: newValue },
+        props.todoListId
+      )
+      dispatch(thunk)
+      // dispatch(updateTaskAC(props.t.id, {title:newValue}, props.todoListId))
+      // props.changeTaskTitle(props.t.id, newValue, props.todoListId)
+    },
+    [props.t.id, props.todoListId]
+  )
+  //key обязательно
+  return (
+    <div
+      key={props.t.id}
+      className={props.t.status === TaskStatuses.New ? 'is-done' : ''}
+    >
+      <Checkbox
+        color={'primary'}
+        checked={props.t.status === TaskStatuses.Completed}
+        onChange={onChangeStatusHandler}
+      />
+      <EditableSpan title={props.t.title} onChange={onChangeTitleHandler} />
 
-    let onChangeStatusHandler = useCallback((event: ChangeEvent<HTMLInputElement>) => {  //  контролируемый чекбокс
-        let newIsDoneValue = event.currentTarget.checked
-        let thunk = updateTaskTC(props.t.id, {status: newIsDoneValue ? TaskStatuses.Completed : TaskStatuses.New}, props.todoListId)
-        dispatch(thunk)
-        // dispatch(changeTaskStatusAC(props.t.id, newIsDoneValue ? TaskStatuses.Completed : TaskStatuses.New, props.todoListId))
-        // props.changeTaskStatus(props.t.id, newIsDoneValue ? TaskStatuses.Completed : TaskStatuses.New, props.todoListId)
-    }, [props.t.id, props.todoListId])
-
-
-    let onChangeTitleHandler = useCallback((newValue: string) => {
-        let thunk = updateTaskTC(props.t.id, {title: newValue}, props.todoListId)
-        dispatch(thunk)
-        // dispatch(updateTaskAC(props.t.id, {title:newValue}, props.todoListId))
-        // props.changeTaskTitle(props.t.id, newValue, props.todoListId)
-    }, [props.t.id, props.todoListId])
-    //key обязательно
-    return <div key={props.t.id} className={props.t.status === TaskStatuses.New ? 'is-done' : ''}>
-
-        <Checkbox color={'primary'} checked={props.t.status === TaskStatuses.Completed}
-                  onChange={onChangeStatusHandler}/>
-        <EditableSpan title={props.t.title} onChange={onChangeTitleHandler}/>
-
-
-        <IconButton onClick={onRemoveHandler}>
-            <Delete/>
-        </IconButton>
+      <IconButton onClick={onRemoveHandler}>
+        <Delete />
+      </IconButton>
     </div>
-
+  )
 })
